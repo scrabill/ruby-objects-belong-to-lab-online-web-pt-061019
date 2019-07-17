30f097ec1b687787d1c9ad438879b8af46e33617
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-17 22:04:48 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,10 @@
+class Artist
+
+  attr_accessor :name # How does the artist and song files (classes) know how to talk to each other
+
+  def initialize # Having a name as an argument is not required for this test
+    @name = name
+  end
+
+
+end
